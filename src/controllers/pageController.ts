3557ff56806e2dd1344e95d43644730f1626c69f
Ana.Sousa <anasousa314@outlook.com>
2021-09-13 15:27:39 -0300

    modificando dados em cada pagina

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,17 +1,39 @@
 import { Request,Response } from "express";
 
 export const home = (req:Request,res:Response)=>{
-  res.render("pages/page");
+
+  res.render("pages/page",{
+    banner:{
+      title: 'Todos os animais',
+      background: 'allanimals'
+    }
+  });
 }
 
 export const dogs = (req:Request,res:Response)=>{
 
+  res.render("pages/page",{
+    banner:{
+      title: 'Cachorros',
+      background: 'banner_dog'
+    }
+  });
 }
 
 export const cats = (req:Request,res:Response)=>{
-
+  res.render("pages/page",{
+    banner:{
+      title: 'Gatos',
+      background: 'banner_cat'
+    }
+  });
 }
 
 export const fishes = (req:Request,res:Response)=>{
-
+  res.render("pages/page",{
+    banner:{
+      title: 'Peixes',
+      background: 'banner_fish'
+    }
+  });
 }
